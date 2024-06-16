b779ed6299e576a9f15616a4b60a4eab92012969
MokotoK <mokotok@protonmail.com>
2024-06-16 15:24:07 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import requests
+import time
+
+BASE_URI = "https://www.bitmex.com/api/v1"
+QUOTE_URI = BASE_URI+"/quote/bucketed"
+
+def getCloses(tf, csv, count):
+    param = {
+    "symbol": "XBTUSD",
+    "binSize": tf,
+    "count": count,
+    "reverse": True
+    }
+    
+
+    response = requests.get(QUOTE_URI, params=param)
+    bitmex = [close for close in response.json()]
+    bitmex.reverse()
+
+    with open(csv, "a") as file:
+        for close in bitmex:
+            file.write(f"{close['timestamp'].split("T")[0]}, {close['timestamp'].split("T")[1].split(".")[0]} ,{round(close['bidPrice'])} \n")
+
+
+getCloses("1h", "bitcoin-1h-closes.csv", 24)
+getCloses("1d", "bitcoin-1d-closes.csv", 1)
+
+
