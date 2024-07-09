c58367d6376e2e3426ab28cad359675ff26ed8e9
MokotoK <mokotok@protonmail.com>
2024-07-09 19:56:55 +1000

    Added 5minute support, fixed bug that occured with multiple double quote marks.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import time
 
 BASE_URI = "https://www.bitmex.com/api/v1"
 QUOTE_URI = BASE_URI+"/quote/bucketed"
@@ -19,10 +18,10 @@ def getCloses(tf, csv, count):
 
     with open(csv, "a") as file:
         for close in bitmex:
-            file.write(f"{close['timestamp'].split("T")[0]}, {close['timestamp'].split("T")[1].split(".")[0]} ,{round(close['bidPrice'])} \n")
+            file.write(f"{close['timestamp'].split('T')[0]}, {close['timestamp'].split('T')[1].split('.')[0]} ,{round(close['bidPrice'])} \n")
 
 
 getCloses("1h", "bitcoin-1h-closes.csv", 24)
 getCloses("1d", "bitcoin-1d-closes.csv", 1)
-
+getCloses("5m", "bitcoin-5m-closes.csv", 288)
 
